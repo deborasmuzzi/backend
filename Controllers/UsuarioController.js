97af4e97ce38c7c6163882c034bf006e43fba816
debora <deboramuzzi@cpejr.com.br>
2025-05-08 20:26:27 -0300

    mensagem

--- Controllers/UsuarioController.js
+++ Controllers/UsuarioController.js
@@ -1,20 +1,42 @@
 const UsuarioModel = require("../Models/UsuarioModel.js");
 
 class UsuarioController {
-   async create(req, res){   
-       const usuario = await UsuarioModel.create(req.body);
+   async create(req, res){ 
+    try {
+        const usuario = await UsuarioModel.create(req.body);
         return res.status(200).json(usuario);
+    } catch (error) {
+        res.status(500).json({message: "Deu ruim aqui", error: error.message })
+    }  
+       
     }
-   async update(req, res){ 
-         const usuarios = await UsuarioModel.find();
-         return res.status(200).json(usuarios);
+     async update(req, res){ 
+        try {
+            const {id} = req.params
+            const usuario = await UsuarioModel.findByIdAndUpdate(id, req.body, {new: true})
+            return res.status(200).json(usuario);
+        } catch (error) {
+            res.status(500).json({message: "Deu ruim aqui", error: error.message })
+        }
+       
     }
-    read(req, res){   
+    async read(req, res){   
+        try {
+            const usuarios = await UsuarioModel.find();
+            return res.status(200).json(usuarios);
+        } catch (error) {
+            res.status(500).json({message: "Deu ruim aqui", error: error.message })
+        }
     } 
     async delete(req, res){   
-        const {id} = req.params
-        await UsuarioModel.findByIdAndDelete(id);
-        return res.status(200).json({"mensagem": "Usuário deletado com sucesso!"});
+        try {
+            const { id } = req.params
+            await UsuarioModel.findByIdAndDelete(id);
+            return res.status(200).json({"mensagem": "Usuário deletado com sucesso!"});
+        } catch (error) {
+            res.status(500).json({message: "Deu ruim aqui", error: error.message })
+        }
+       
     }
 }
 
--- routes.js
+++ routes.js
@@ -1,14 +1,17 @@
 const { Router } = require ("express");
 const UsuarioController = require ("./Controllers/UsuarioController");
 const SessoesController = require("./Controllers/SessoesController");
+const UsuarioValidator = require ("./Validators/UsuarioValidator.js");
+const SessosesValidator = require ("./Validators/SessoesValidator.js");
 
 const rotas = Router();
 //usuario
-rotas.post('/usuarios', UsuarioController.create);
+rotas.post('/usuarios', UsuarioValidator.create, UsuarioController.create);
 rotas.get('/usuarios', UsuarioController.read);
-rotas.delete('/usuarios/:id', UsuarioController.delete);
+rotas.delete('/usuarios/:id',  UsuarioValidator.destroy, UsuarioController.delete);
+rotas.put('/usuarios/:id', UsuarioValidator.update, UsuarioController.update);
 //sessoes
-rotas.post('/sessoes', SessoesController.create);
+rotas.post('/sessoes', SessosesValidator.create, SessoesController.create);
 rotas.get('/sessoes', SessoesController.read);
-rotas.delete('/sessoes/:id', SessoesController.delete);
+rotas.delete('/sessoes/:id', SessosesValidator.destroy, SessoesController.delete);
 module.exports = rotas;
